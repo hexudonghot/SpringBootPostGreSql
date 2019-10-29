2225ed3c1555776771c4241b81d417a76e2ddc68
何旭東 <>
2019-10-29 19:02:03 +0800

    游标手动控制事务

--- src/main/java/org/spring/springboot/controller/UserRestController.java
+++ src/main/java/org/spring/springboot/controller/UserRestController.java
@@ -4,10 +4,8 @@ import org.spring.springboot.domain.City;
 import org.spring.springboot.domain.User;
 import org.spring.springboot.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * 用户控制层
@@ -19,16 +17,18 @@ public class UserRestController {
 
     @Autowired
     private UserService userService;
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
 
     /**
      * 根据用户名获取用户信息，包括从库的地址信息
      *
-     * @param userName
      * @return
      */
     @RequestMapping(value = "/api/user", method = RequestMethod.GET)
-    public User findByName(@RequestParam(value = "userName", required = true) String userName) {
-        return userService.findByName(userName);
+    public User findByName() {
+         kafkaTemplate.send("fddffffff", "message");
+        return null;
     }
 
     /**
--- src/main/java/org/spring/springboot/mq/MsgConsumer.java
+++ src/main/java/org/spring/springboot/mq/MsgConsumer.java
@@ -1,11 +1,10 @@
 package org.spring.springboot.mq;
 
-import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-import java.util.Optional;
 
 /**
  * 描述:消息消费者
@@ -13,13 +12,22 @@ import java.util.Optional;
  * @author yanpenglei
  * @create 2017-10-16 18:33
  **/
+//@Service
+//class MsgConsumer {
+//
+//    @KafkaListener(topics = {"fddffffff"})
+//    public void listen(String content) {
+//        System.out.println("消息被消费"+content);
+//    }
+//
+//}
+
+
 @Component
 public class MsgConsumer {
 
-    @KafkaListener(topics = {"topic-1","topic-2"})
-    public void processMessage(String content) {
-
-        System.out.println("消息被消费"+content);
+    @KafkaListener(groupId = "simpleGroup",topics = { "fddffffff" })
+    public void consumer(ConsumerRecord<String, String> consumerRecord) {
+        System.out.println("消息消费--》" + consumerRecord.value());
     }
-
 }
