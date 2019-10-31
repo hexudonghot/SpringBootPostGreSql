8506836a722f68f615cc976a3a3e0426e44c86a8
何旭東 <>
2019-10-31 16:44:57 +0800

    kafka停机

--- src/main/java/org/spring/springboot/mq/MsgConsumer.java
+++ src/main/java/org/spring/springboot/mq/MsgConsumer.java
@@ -1,6 +1,9 @@
 package org.spring.springboot.mq;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.spring.springboot.service.impl.UserServiceImpl;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
@@ -8,9 +11,9 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class MsgConsumer {
-
+    private static final Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
     @KafkaListener(groupId = "simpleGroup",topics = { "fddffffff" })
     public void consumer(ConsumerRecord<String, String> consumerRecord) {
-        System.out.println("消息消费--》" + consumerRecord.value());
+        logger.info("消息消费--》" + consumerRecord.value());
     }
 }
