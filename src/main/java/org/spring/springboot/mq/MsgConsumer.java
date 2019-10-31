67a4afe81697307fae9f97fbb2188f561c80cdf4
何旭東 <>
2019-10-31 15:15:11 +0800

    整合kafka

--- src/main/java/org/spring/springboot/mq/MsgConsumer.java
+++ src/main/java/org/spring/springboot/mq/MsgConsumer.java
@@ -3,24 +3,7 @@ package org.spring.springboot.mq;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
-
-/**
- * 描述:消息消费者
- *
- * @author yanpenglei
- * @create 2017-10-16 18:33
- **/
-//@Service
-//class MsgConsumer {
-//
-//    @KafkaListener(topics = {"fddffffff"})
-//    public void listen(String content) {
-//        System.out.println("消息被消费"+content);
-//    }
-//
-//}
 
 
 @Component
