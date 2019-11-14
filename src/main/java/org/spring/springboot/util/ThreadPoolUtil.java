32eb3ac325f511056cd4112de5bf95cd322254e6
hxd <job_finder@126.com>
2019-11-14 22:50:48 +0800

    ...

--- src/main/java/org/spring/springboot/util/ThreadPoolUtil.java
+++ src/main/java/org/spring/springboot/util/ThreadPoolUtil.java
@@ -6,18 +6,45 @@ public final class ThreadPoolUtil {
 
     private ThreadPoolUtil() {}
 
-    private static final int queueSize = 20;
-    private static final int corePoolSize = 10;
-    private static final int maximumPoolSize = 10;
-    private static final int keepAliveTime = 10;
+    private static final int queueSize = 2;
+    private static final int corePoolSize = 1;
+    private static final int maximumPoolSize = 1;
+    private static final int keepAliveTime = 1;
     private static final BlockingQueue<Runnable> queue = new LinkedBlockingQueue<>(queueSize);
-    private static final ThreadPoolExecutor executor = new ThreadPoolExecutor(
-            corePoolSize, maximumPoolSize, keepAliveTime, TimeUnit.SECONDS,
-            queue, new ThreadPoolExecutor.CallerRunsPolicy());
+
+    private static final ThreadPoolExecutor executor = new ThreadPoolExecutor(corePoolSize, maximumPoolSize, keepAliveTime, TimeUnit.SECONDS,
+            new LinkedBlockingQueue<>(queue), (r, executor) -> {
+                if (!executor.isShutdown()) {
+                    try {
+                        executor.getQueue().put(r);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
 
     public static void executor(Runnable command) {
         executor.execute(command);
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
    // private static final int  aa= ((LinkedBlockingQueue) ThreadPoolUtil.queue).size();
 
